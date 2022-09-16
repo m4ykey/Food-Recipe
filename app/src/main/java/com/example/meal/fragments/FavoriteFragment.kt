71bc888bfb236f1714c6c314fcfd11c288cd744f
kfjdj <kfjdjdkh@gmail.com>
2022-09-16 22:26:20 +0200

    favorite fragment delete item

--- app/src/main/java/com/example/meal/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/example/meal/fragments/FavoriteFragment.kt
@@ -1,17 +1,22 @@
 package com.example.meal.fragments
 
+import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.meal.R
 import com.example.meal.adapter.SearchFavAdapter
 import com.example.meal.databinding.FragmentFavoriteBinding
 import com.example.meal.viewmodel.FoodViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,6 +39,10 @@ class FavoriteFragment : Fragment() {
 
         setupRecyclerView()
         viewModel.getAllFood().observe(viewLifecycleOwner) { favorite ->
+            if (favorite.isEmpty()){
+                binding.emptyContainer.root.visibility = View.VISIBLE
+                return@observe
+            }
             favAdapter.differ.submitList(favorite)
         }
 
@@ -41,6 +50,34 @@ class FavoriteFragment : Fragment() {
             findNavController().navigateUp()
         }
 
+        favAdapter.setOnItemClick {
+            val bundle = Bundle().apply {
+                putParcelable("food", it)
+            }
+            findNavController().navigate(R.id.action_favoriteFragment_to_detailFragment, bundle)
+        }
+
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.LEFT, ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val deleteItem = favAdapter.differ.currentList[position]
+                viewModel.delete(deleteItem)
+
+                Snackbar.make(view, "Removed from favorite", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.rvFavorite)
     }
 
     private fun setupRecyclerView() {
